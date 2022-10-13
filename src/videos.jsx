8a6afb37c00bbf4fcbf194597d6678d598ce5af2
joengyamba <joycebaraka97@gmail.com>
2022-10-13 11:58:28 +0100

    affichage video

--- src/videos.jsx
+++ src/videos.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./headerstyle.css";
+import { useEffect, useState } from "react";
 export default function Videos() {
+  const [videos, setVideos] = useState([]);
+  const token = localStorage.getItem("token");
+  const clé = "AIzaSyCFR0BUmDJEn_6lDXEy364ieGsVz7s3kEk";
+  const fetchVideo = ` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=US&key=${clé}`;
+  useEffect(() => {
+    fetch(fetchVideo)
+      .then((response) => response.json())
+      .then((data) => setVideos(data.items));
+  }, [token]);
+  console.log(videos);
   return (
-    <>
-      <div className="video-list">
+    <div className="video-list">
+      {videos.map((item) => (
         <div className="content-video">
-          <p>Mes Videos</p>
-          <img src=""></img>
+          <img src={item.snippet.thumbnails.default.url}></img>
+          <p>{item.snippet.title}</p>
         </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
